9646b8bee8b1b661a52d01a0ec3efc760c237434
Ricardo Medina <ricki9@gmail.com>
2021-01-15 17:57:32 -0600

    Added Generic arrayList tests

--- src/main/java/jtraining/test/collections/ListTests.java
+++ src/main/java/jtraining/test/collections/ListTests.java
@@ -8,15 +8,25 @@ import java.util.List;
 
 public class ListTests {
 
-    private void printList(List list){
+    private void printList(List list) {
+        printList(list, null);
+    }
+
+    private void printList(List list, String title){
+        if(title != null){
+            System.out.println("== " + title + " ==");
+        }
         for(int i = 0; i < list.size(); i++){
             var item = list.get(i);
             System.out.println("List[" + i +"](" + item.getClass() + "): " + item);
         }
     }
 
+    /*
+    * Test ArrayList Print
+    * */
     @Test
-    public void ArrayListTest_printItems(){
+    public void ArrayListTest_print(){
         ArrayList arrayList = new ArrayList();
         arrayList.add("Hello");
         arrayList.add(10);
@@ -25,4 +35,20 @@ public class ListTests {
         printList(arrayList);
     }
 
+    /*
+    * Test ArrayList Generic Print
+    * */
+    @Test
+    public void ArrayListTest_genericPrint(){
+        ArrayList<String> stringList = new ArrayList<>();
+        stringList.add("Hello");
+        stringList.add("World");
+        printList(stringList, "stringList");
+
+        ArrayList<Integer> integerList = new ArrayList<>();
+        integerList.add(10);
+        integerList.add(20);
+        integerList.add(30);
+        printList(integerList, "integerList");
+    }
 }
